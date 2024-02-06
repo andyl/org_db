792fe4f72057e9a01a3af0d124ffcb9014153622
andyl <andy@r210.com>
2024-02-05 21:41:52 -0800

    Enable database transaction

--- lib/mix/tasks/test/t6.ex
+++ lib/mix/tasks/test/t6.ex
@@ -2,18 +2,18 @@ defmodule Mix.Tasks.Test.T6 do
   use Mix.Task
 
   alias OrgDb.Doc.Dir
-  # alias OrgDb.SearchFts.Db
+  alias OrgDb.Fts.Db
 
   def run(_) do
-    # data =
+    data =
       "/home/aleak/util/org"
       |> Dir.ingest()
       |> List.flatten()
 
-    # db = Db.open()
-    # Db.load(db, data)
-    #
-    # Db.search(db, "Reflow")
-    # |> IO.inspect()
+    db = Db.open()
+    Db.load(db, data)
+
+    Db.search(db, "Reflow")
+    |> IO.inspect()
   end
 end
